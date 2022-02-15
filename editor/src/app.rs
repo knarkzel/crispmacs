97b6f36bace05f6b358b4b33f79bf17e100e0324
Knarkzel <knarkzel@gmail.com>
2022-02-15 03:00:28 +0100

    basic eval, interaction, etc

--- editor/src/app.rs
+++ editor/src/app.rs
@@ -1,8 +1,11 @@
+use crisp::Context;
 use eframe::{egui::*, epi};
 
 #[derive(Default)]
 pub struct Editor {
     input: String,
+    context: Context,
+    output: String,
 }
 
 impl epi::App for Editor {
@@ -17,9 +20,9 @@ impl epi::App for Editor {
     fn setup(&mut self, ctx: &CtxRef, _: &epi::Frame, _: Option<&dyn epi::Storage>) {
         // Proper font
         let mut fonts = FontDefinitions::default();
-        fonts
-            .family_and_size
-            .insert(TextStyle::Monospace, (FontFamily::Proportional, 48.0));
+        for font in fonts.family_and_size.iter_mut() {
+            *font.1 = (FontFamily::Proportional, 48.0);
+        }
         ctx.set_fonts(fonts);
 
         // Light mode
@@ -28,15 +31,35 @@ impl epi::App for Editor {
 
     fn update(&mut self, ctx: &CtxRef, _: &epi::Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            ScrollArea::vertical().show(ui, |ui| {
-                ui.add(
-                    TextEdit::multiline(&mut self.input)
-                        .code_editor()
-                        .frame(false)
-                        .desired_rows(usize::MAX)
-                        .desired_width(f32::INFINITY),
-                );
-            })
+            if ui.button("Evaluate").clicked() {
+                match crisp::parse_and_eval(&self.input, &mut self.context) {
+                    Ok(Ok(exprs)) => {
+                        self.output = exprs
+                            .into_iter()
+                            .map(|it| format!("{it}"))
+                            .collect::<String>()
+                    }
+                    Ok(Err(error)) => self.output = format!("Evaluation error: {error}"),
+                    Err(error) => self.output = format!("Parsing error: {error}"),
+                    _ => {}
+                }
+            }
+
+            if self.output.len() > 0 {
+                Window::new("Output").auto_sized().show(ctx, |ui| {
+                    ScrollArea::vertical().show(ui, |ui| {
+                        ui.label(&self.output);
+                    })
+                });
+            }
+
+            ui.add(
+                TextEdit::multiline(&mut self.input)
+                    .code_editor()
+                    .frame(false)
+                    .desired_rows(30)
+                    .desired_width(f32::INFINITY),
+            );
         });
     }
 }
